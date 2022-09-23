7f645f5732c49eb07b4f7abdbe250e442115a38b
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-09-23 13:25:00 +0300

    add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 import './header.css';
 import { Container, Row } from 'reactstrap';
 import logo from '../../assets/logo.png';
+import userIcon from '../../assets/user-icon.png';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineShoppingBag } from 'react-icons/hi';
+import { AiOutlineHeart, AiOutlineMenu } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+const nav__links = [
+  {
+    path: 'home',
+    display: 'Home',
+  },
+  {
+    path: 'shop',
+    display: 'Shop',
+  },
+  {
+    path: 'cart',
+    display: 'Cart',
+  },
+];
 
 const Header = () => {
   return (
@@ -13,13 +33,42 @@ const Header = () => {
               <img src={logo} alt='logo' />
               <div>
                 <h1>OnlineStore</h1>
-                <p>Since 2000</p>
+                {/* <p>Since 2000</p> */}
               </div>
             </div>
             <div className='navigation'>
               <ul className='menu'>
-                <li className='nav__item'></li>
+                {nav__links.map((item, index) => (
+                  <li className='nav__item' key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? 'nav__active' : ''
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
               </ul>
+            </div>
+            <div className='nav__icons'>
+              <span className='fav__icon'>
+                <AiOutlineHeart size={24} />
+                <span className='badge'>1</span>
+              </span>
+              <span className='cart__icon'>
+                <HiOutlineShoppingBag size={24} />
+                <span className='badge'>1</span>
+              </span>
+              <span>
+                <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt='' />
+              </span>
+            </div>
+            <div className='mobile__menu'>
+              <span>
+                <AiOutlineMenu size={24} />
+              </span>
             </div>
           </div>
         </Row>
