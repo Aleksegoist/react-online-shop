7cde69690cf2b2970562564418ec22fdbdc1f62f
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-10-02 20:12:11 +0300

    add search category

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,10 +5,65 @@ import { Col, Container, Row } from 'reactstrap';
 
 import Helmet from '../components/helmet/Helmet';
 import CommonSection from '../components/ui/CommonSection';
+import ProductsList from '../components/ui/ProductsList';
+
+import products from '../data/product';
 
 import { AiOutlineSearch } from 'react-icons/ai';
+import { useState } from 'react';
 
 const Shop = () => {
+  const [productsData, setProductsData] = useState(products);
+
+  const handleFilter = (e) => {
+    const filterValue = e.target.value;
+    if (filterValue === 'trainers') {
+      const filteredProducts = products.filter(
+        (item) => item.category === 'trainers'
+      );
+      setProductsData(filteredProducts);
+    }
+
+    if (filterValue === 'mobile') {
+      const filteredProducts = products.filter(
+        (item) => item.category === 'mobile'
+      );
+      setProductsData(filteredProducts);
+    }
+
+    if (filterValue === 'shirts') {
+      const filteredProducts = products.filter(
+        (item) => item.category === 'shirts'
+      );
+      setProductsData(filteredProducts);
+    }
+
+    if (filterValue === 'wireless') {
+      const filteredProducts = products.filter(
+        (item) => item.category === 'wireless'
+      );
+      setProductsData(filteredProducts);
+    }
+
+    if (filterValue === 'watch') {
+      const filteredProducts = products.filter(
+        (item) => item.category === 'watch'
+      );
+      setProductsData(filteredProducts);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+
+    const searchedProducts = products.filter((item) =>
+      item.productName
+        .toLocaleLowerCase()
+        .includes(searchTerm.toLocaleLowerCase())
+    );
+    setProductsData(searchedProducts);
+  };
+
   return (
     <Helmet title='Shop'>
       <CommonSection title='Products' />
@@ -17,9 +72,9 @@ const Shop = () => {
           <Row>
             <Col lg='3' md='3'>
               <div className='filter__widget'>
-                <select>
+                <select onChange={handleFilter}>
                   <option>Filter By Category</option>
-                  <option value='sneakers'>Sneakers</option>
+                  <option value='trainers'>Trainers</option>
                   <option value='mobile'>Mobile</option>
                   <option value='shirts'>Shirts</option>
                   <option value='wireless'>Wireless</option>
@@ -38,12 +93,27 @@ const Shop = () => {
             </Col>
             <Col lg='6' md='6'>
               <div className='search__box'>
+                <input
+                  type='text'
+                  placeholder='Search...'
+                  onChange={handleSearch}
+                />
                 <span>
-                  <AiOutlineSearch />
+                  <AiOutlineSearch size={24} />
                 </span>
-                <input type='text' placeholder='Search...' />
               </div>
             </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className='pt-0'>
+        <Container>
+          <Row>
+            {productsData.length === 0 ? (
+              <h1 className='text-center fs-4'>No products are found!</h1>
+            ) : (
+              <ProductsList data={productsData} />
+            )}
           </Row>
         </Container>
       </section>
