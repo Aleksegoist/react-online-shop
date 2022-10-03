048e7742e0c66d117a084505f9295fb63398c150
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-10-03 16:18:55 +0300

    add ProductDetails

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -6,13 +6,20 @@ import products from '../data/product';
 
 import Helmet from '../components/helmet/Helmet';
 import CommonSection from '../components/ui/CommonSection';
+import ProductsList from '../components/ui/ProductsList';
 
 import { Col, Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 
 import { RiStarFill, RiStarHalfFill } from 'react-icons/ri';
 
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+
 const ProductDetails = () => {
+  const [tab, setTab] = useState('desc');
+  const [rating, setRating] = useState(null);
+
   const { id } = useParams();
   const product = products.find((item) => item.id === id);
 
@@ -21,14 +28,17 @@ const ProductDetails = () => {
     productName,
     price,
     avgRating,
-    review,
+    reviews,
     description,
     shortDesc,
+    category,
   } = product;
 
+  const relatedProducts = products.filter((item) => item.category === category);
+
   return (
-    <Helmet>
-      <CommonSection />
+    <Helmet title={productName}>
+      <CommonSection title={productName} />
 
       <section className='pt-10'>
         <Container>
@@ -40,31 +50,131 @@ const ProductDetails = () => {
             <Col lg='6'>
               <div className='product__details'>
                 <h2>{productName}</h2>
-                <p>{shortDesc}</p>
+                <p className='mt-3'>{shortDesc}</p>
                 <div className='product__rating d-flex align-items-center gap-5 mb-3'>
                   <div>
-                    <span>
+                    <span onClick={() => setRating(1)}>
                       <RiStarFill size={24} style={{ color: 'gold' }} />
                     </span>
-                    <span>
+                    <span onClick={() => setRating(2)}>
                       <RiStarFill size={24} style={{ color: 'gold' }} />
                     </span>
-                    <span>
+                    <span onClick={() => setRating(3)}>
                       <RiStarFill size={24} style={{ color: 'gold' }} />
                     </span>
-                    <span>
+                    <span onClick={() => setRating(4)}>
                       <RiStarFill size={24} style={{ color: 'gold' }} />
                     </span>
-                    <span>
+                    <span onClick={() => setRating(5)}>
                       <RiStarHalfFill size={24} style={{ color: 'gold' }} />
                     </span>
                   </div>
                   <span>({avgRating} ratings)</span>
                 </div>
-                <span>${price}</span>
-                <button className='buy__btn'>Add To Cart</button>
+                <div className='d-flex align-items-center gap-5'>
+                  <p className='product__price'>${price}</p>
+                  <p>Category: {category.toUpperCase()}</p>
+                </div>
+                <motion.button whileHover={{ scale: 1.1 }} className='buy__btn'>
+                  Add To Cart
+                </motion.button>
               </div>
             </Col>
+          </Row>
+        </Container>
+      </section>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              <div className='tab__wrapper d-flex align-items-center gap-5'>
+                <h6
+                  className={`${tab === 'desc' ? 'active__tab' : ''}`}
+                  onClick={() => setTab('desc')}
+                >
+                  Description
+                </h6>
+                <h6
+                  className={`${tab === 'rev' ? 'active__tab' : ''}`}
+                  onClick={() => setTab('rev')}
+                >
+                  Reviews ({reviews.length})
+                </h6>
+              </div>
+
+              {tab === 'desc' ? (
+                <div className='tab__content mt-5'>
+                  <p>{description}</p>
+                </div>
+              ) : (
+                <div className='product__review mt-5'>
+                  <div className='review__wrapper'>
+                    <ul>
+                      {reviews?.map((item, index) => (
+                        <li key={index} className='mb-4'>
+                          <h6>Alex Pretender</h6>
+                          <span>{item.rating} (rating)</span>
+                          <p>{item.text}</p>
+                        </li>
+                      ))}
+                    </ul>
+                    <div className='review__form'>
+                      <h4>Leave your experience</h4>
+                      <form action=''>
+                        <div className='form__group'>
+                          <input type='text' placeholder='Enter your name' />
+                        </div>
+
+                        <div className='form__group d-flex align-items-center gap-5'>
+                          <span>
+                            1
+                            <RiStarFill />
+                          </span>
+                          <span>
+                            2
+                            <RiStarFill />
+                          </span>
+                          <span>
+                            3
+                            <RiStarFill />
+                          </span>
+                          <span>
+                            4
+                            <RiStarFill />
+                          </span>
+                          <span>
+                            5
+                            <RiStarFill />
+                          </span>
+                        </div>
+
+                        <div className='form__group'>
+                          <textarea
+                            rows={4}
+                            type='text'
+                            placeholder='Review message...'
+                          />
+                        </div>
+
+                        <motion.button
+                          type='submit'
+                          whileHover={{ scale: 1.1 }}
+                          className='buy__btn'
+                        >
+                          Submit
+                        </motion.button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </Col>
+
+            <Col lg='12' className='mt-5'>
+              <h2 className='related__title'>You might also like</h2>
+            </Col>
+
+            <ProductsList data={relatedProducts} />
           </Row>
         </Container>
       </section>
