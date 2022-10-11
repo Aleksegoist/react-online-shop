5da1e37bc5dee883f263935f41698eaab4723bb7
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-10-11 18:35:24 +0300

    add

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Helmet from '../components/helmet/Helmet';
+import CommonSection from '../components/ui/CommonSection';
+
 const Checkout = () => {
   return <div>Checkout</div>;
 };
