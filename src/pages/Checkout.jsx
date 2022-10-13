de5f1d254e860ede72a1ed0a9419ac6a8bb79de3
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-10-13 13:27:09 +0300

    add checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -6,7 +6,14 @@ import { Col, Container, Form, FormGroup, Row } from 'reactstrap';
 import Helmet from '../components/helmet/Helmet';
 import CommonSection from '../components/ui/CommonSection';
 
+import { motion } from 'framer-motion';
+
+import { useSelector } from 'react-redux';
+
 const Checkout = () => {
+  const totalQty = useSelector((state) => state.cart.totalQuantity);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+
   return (
     <Helmet title='Checkout'>
       <CommonSection title='CheckOut' />
@@ -49,11 +56,27 @@ const Checkout = () => {
             <Col lg='4'>
               <div className='checkout__cart'>
                 <h6>
-                  Total: <span>0</span>
+                  Total Qty: <span>{totalQty} items</span>
                 </h6>
                 <h6>
-                  Total: <span>0</span>
+                  Subtotal: <span>${totalAmount}</span>
                 </h6>
+                <h6>
+                  <span>
+                    Shipping: <br />
+                    free shipping
+                  </span>
+                  <span>${totalAmount}</span>
+                </h6>
+                <h4>
+                  Total Cost: <span>$120</span>
+                </h4>
+                <motion.button
+                  whileTap={{ scale: 1.2 }}
+                  className='buy__btn auth__btn w-100'
+                >
+                  Place an order
+                </motion.button>
               </div>
             </Col>
           </Row>
