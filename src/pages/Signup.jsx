257c1a1ac9545fe8b0a14c35db4ea04062e1db09
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-10-13 14:10:32 +0300

    add form login and signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import '../styles/login.css';
+
+import { Link } from 'react-router-dom';
+import { Col, Container, Form, FormGroup, Row } from 'reactstrap';
+
+import Helmet from '../components/helmet/Helmet';
+import { useState } from 'react';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <Helmet title='Signup'>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' className='m-auto text-center'>
+              <h3 className='fw-bold mb-4'>Signup</h3>
+
+              <Form className='auth__form'>
+                <FormGroup className='form__group'>
+                  <input
+                    type='email'
+                    placeholder='Enter your email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className='form__group'>
+                  <input
+                    type='password'
+                    placeholder='Enter your password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+
+                <button type='submit' className='buy__btn login__btn'>
+                  Create an Account
+                </button>
+                <p>
+                  Already have an account?
+                  <Link to='/login'>Enter your login</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Signup;
